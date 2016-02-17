39a6e014cf94d2c4460820f276ddac65b8503fd1
Preston Mueller <skierjunk@gmail.com>
2016-02-16 21:53:55 -0500

    bag class

---
+++ bag.py
@@ -0,0 +1,19 @@
+# Project 5 - CSP
+# Theresa
+
+class Bag:
+
+    def __init__(self):
+        self.name = ""
+        self.capacity = 0
+        self.count = 0
+        self.contains = []
+
+    def weight():
+        pass
+
+    def wastedCapacity():
+        pass
+
+    def addItem(type, number):
+        pass

f4fbdb83c888088c7a673b791d87aa92f56830eb
Theresa Inzerillo <theresa.inzerillo@gmail.com>
2016-02-16 21:39:29 -0500

    initial commit

---
+++ csp.py
@@ -0,0 +1,19 @@
+# csp.py
+
+def parseInput() {
+	
+}
+
+def within_limits(bg, n) {
+	return 
+}
+
+def can_be_in_bag(bg, x) {
+	dict[x]
+}
+
+bag_min=0
+bag_max=0
+binaryequals[]
+binarynotequals[]
+

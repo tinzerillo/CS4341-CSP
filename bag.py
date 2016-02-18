87dfd24e242ed52711be67549dd2e3689d26dc14
Theresa Inzerillo <theresa.inzerillo@gmail.com>
2016-02-18 16:16:16 -0500

    count to weight

--- bag.py
+++ bag.py
@@ -6,8 +6,8 @@ class Bag:
 	def __init__(self, name, capacity):
 		self._name = name
 		self._capacity = capacity
-		self._count = 0
-		self._contains = []
+		self._weight = 0
+		self._contains = [] 
 
 	@property
 	def name(self):
@@ -18,25 +18,22 @@ class Bag:
 		return self._capacity
 		
 	@property
-	def count(self):
-		return self._count
+	def weight(self):
+		return self._weight
 			
 	@property
 	def contains(self):
 		return self._contains
 
-	def weight(self):
-		pass
-
 	def wastedCapacity(self):
-		return self.capacity-self.count
+		return self.capacity-self.weight
 
 	def addItem(self, type, number):
 		if type not in self._contains:
 			self._contains.append(type)
-		self._count += number
+		self._weight += number
 		
 	def removeItem(self, type, number):
 		if type in self.contains:
 			self.contains.remove(type)
-		self.count -= number
+		self.weight -= number

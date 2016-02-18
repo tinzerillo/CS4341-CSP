e857d9bed6e57e07c81c222dd06960476a2c0ea4
Preston Mueller <skierjunk@gmail.com>
2016-02-18 00:48:28 -0500

    Preston did isCSPcomplete

--- csp.py
+++ csp.py
@@ -14,7 +14,7 @@ class InputType(Enum):
 	binarynoteq=7
 	binarysimult=8
 
-# constraints 
+# constraints
 items = {}
 bag_min=0
 bag_max=0
@@ -25,12 +25,12 @@ un_excl = {}
 bin_sim = {}
 
 bags = []
-	
+
 def parseInput(file):
 	f = open(file, "r")
 	type=0
 	line = f.readline()
-	
+
 	while line:
 		if line[0] is '#':
 			type+=1
@@ -55,58 +55,148 @@ def parseInput(file):
 			elif type is 8:
 				bin_sim[line[0]+line[1]]=line[2]+line[3]
 			line = f.readline()
-		
+
 	f.close()
 
 
 def within_limits(bag, n):
 	return bag.count + n <= bag_max and bag.count + n >= bag_min
-	
+
 def canAddToBag(item, bag):
 	if bag_max is 0:
 		return bag.capacity - bag.count >= items[item]
 	else:
 		return within_limits(bag, items[item])
-	
+
 def isCSPcomplete(assignment):
-	return False
+
+	# Fit limits
+	for bag in assignment:
+		if bag.count < (bag.capacity * 0.9):
+			return False
+
+	# Unary inclusive
+	for constraint in un_incl.items():
+		variable = constraint[0]
+		# find what bag that variable is in...
+		target_bag = None
+		for bag in assignment:
+			if variable in bag.contains:
+				target_bag = bag
+				break
+
+		if target_bag not in constraint[1]:
+			return False
+		else:
+			return True
+
+	#Unary exclusive
+
+	for constraint in un_excl.items():
+		variable = constraint[0]
+		# find what bag that variable is in...
+		target_bag = None
+		for bag in assignment:
+			if variable in bag.contains:
+				target_bag = bag
+				break
+
+		if target_bag in constraint[1]:
+			return False
+		else:
+			return True
+
+
+	#Binary constraints
+
+		#Equal
+	for constraint in binaryequals.items():
+		variableOne = constraint[0][1]
+		variableTwo = constraint[0][1]
+
+
+		for bag in assignment:
+			if variableOne in bag.contains:
+				if variableTwo not in bag.contains:
+					return False
+
+
+	#Not equal
+	for constraint in binarynotequals.items():
+		variableOne = constraint[0][1]
+		variableTwo = constraint[0][1]
+
+		for bag in assignment:
+			if variableOne in bag.contains:
+				if variableTwo in bag.contains:
+					return False
+
+
+	#Binary simultaneous
+	for constraint in bin_sim.items():
+		variableOne = constraint[0][0]
+		variableTwo = constraint[0][1]
+
+		bagOne = constraint[1][0]
+		bagTwo = constraint[1][1]
+
+		bagOneClass = None
+		bagTwoClass = None
+
+		for bag in assignment:
+			if bag.name == bagOne:
+				bagOneClass = bag
+			elif bag.name == bagTwo:
+				bagTwoClass = bag
+
+		if variableOne in bagOneClass.contains and variableTwo not in bagTwoClass.contains:
+			return False
+
+		if variableOne in bagTwoClass.contains and variableTwo not in bagOneClass.contains:
+			return False
+
+		if variableTwo in bagOneClass.contains and variableOne not in bagTwoClass.contains:
+			return False
+
+		if variableTwo in bagTwoClass.contains and variableOne not in bagOneClass.contains:
+			return False
+
+	return True
 
 def nextUnassignedVariables(assignment):
 	#assignment: [] of bags
 	variables = list(items.keys())
-	
+
 	for b in assignment:
 		print(b.contains)
 		for variable in b.contains:
 			variables.remove(variable)
 		if len(variables) is 0:
 			return []
-			
-	return variables	
+
+	return variables
 
 def Backtrack(assignment):
 	if isCSPcomplete is True:
-		return 
-	
+		return
+
 	if len(nextUnassignedVariables(assignment)) is 0:
 		return
-		
+
 	var = nextUnassignedVariables(assignment)[0]
-		
+
 	for bag in assignment:
 		if canAddToBag(var, bag) is True:
 			bag.addItem(var, items[var])
 		else:
 			i+=1
 			Backtrack(assignment)
-	
+
 if len(sys.argv) != 2:
 	print("Proper usage is python csp.py inputfile")
 	exit()
-	
+
 parseInput(sys.argv[1])
 Backtrack(bags)
 #for b in bags:
 	#print(b.contains)
-	
-
